b161d8857ace0561e94a860b1b22201ebbd6adb7
Feri Fajri <feri.okana@gmail.com>
2020-06-04 12:01:02 +0700

    Initial Commit

---
+++ ARPlanets/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARPlanets
+//
+//  Created by Feri Fajri on 04/06/20.
+//  Copyright © 2020 Feri Fajri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
